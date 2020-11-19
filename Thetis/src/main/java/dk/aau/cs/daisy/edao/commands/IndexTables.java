89369a375abf067f11113e0f092c2267a9e97a27
Matteo Lissandrini <matteo@cs.aau.dk>
2020-11-19 13:58:52 +0100

    neo4j connection

--- Thetis/src/main/java/dk/aau/cs/daisy/edao/commands/IndexTables.java
+++ Thetis/src/main/java/dk/aau/cs/daisy/edao/commands/IndexTables.java
@@ -1,8 +1,11 @@
 package dk.aau.cs.daisy.edao.commands;
 
+import dk.aau.cs.daisy.edao.connector.Neo4j;
 import picocli.CommandLine;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @picocli.CommandLine.Command(name = "index", description = "creates the index for the specified set of tables")
 public class IndexTables extends Command {
@@ -11,7 +14,7 @@ public class IndexTables extends Command {
     CommandLine.Model.CommandSpec spec; // injected by picocli
 
     /**
-     * java -jar Thetis.1.0.jar  index  --type wikitables --tables  data/tables/wikitables --output data/index/wikitables
+     * java -jar Thetis.1.0.jar  index --config ./config.properties --type wikitables --tables  data/tables/wikitables --output data/index/wikitables
      */
     public enum TableType {
         WIKI("wikitables"),
@@ -29,12 +32,32 @@ public class IndexTables extends Command {
     }
 
 
-    @CommandLine.Option(names = { "-tt", "--table-type" }, description = "Table types: ${COMPLETION-CANDIDATES}")
+    @CommandLine.Option(names = { "-tt", "--table-type" }, description = "Table types: ${COMPLETION-CANDIDATES}", required = true)
     private TableType tableType = null;
 
 
+
+    private File configFile = null;
+    @CommandLine.Option(names = { "-cf", "--config"}, paramLabel = "CONF", description = "configuration file", required = true, defaultValue = "./config.properties" )
+    public void setConfigFile(File value) {
+
+        if(!value.exists()){
+            throw new CommandLine.ParameterException(spec.commandLine(),
+                    String.format("Invalid value '%s' for option '--config': " +
+                            "the file does not exists.", value));
+        }
+
+        if (value.isDirectory()) {
+            throw new CommandLine.ParameterException(spec.commandLine(),
+                    String.format("Invalid value '%s' for option '--config': " +
+                            "the path should point to a file not to a directory.", value));
+        }
+        tableDir = value;
+    }
+
+
     private File tableDir = null;
-    @CommandLine.Option(names = { "-td", "--table-dir"}, paramLabel = "TABLE_DIR", description = "Directory containing tables")
+    @CommandLine.Option(names = { "-td", "--table-dir"}, paramLabel = "TABLE_DIR", description = "Directory containing tables", required = true)
     public void setTableDirectory(File value) {
 
         if(!value.exists()){
@@ -53,7 +76,7 @@ public class IndexTables extends Command {
 
 
     private File outputDir = null;
-    @CommandLine.Option(names = { "-od", "--output-dir" }, paramLabel = "OUT_DIR", description = "Directory where to save the index")
+    @CommandLine.Option(names = { "-od", "--output-dir" }, paramLabel = "OUT_DIR", description = "Directory where to save the index", required = true)
     public void setOutputDirectory(File value) {
 
         if(!value.exists()){
@@ -87,6 +110,17 @@ public class IndexTables extends Command {
 
         System.out.println("Input Directory: " + this.tableDir.getAbsolutePath() );
         System.out.println("Output Directory: " + this.outputDir.getAbsolutePath() );
+
+        try {
+            Neo4j connector = new Neo4j(this.configFile);
+            connector.testConnection();
+        } catch (FileNotFoundException ex){
+            System.err.println( "Configuration file for Neo4j connector not found");
+            System.err.println(ex.getMessage());
+        } catch ( IOException ex){
+            System.err.println("Error in reading configuration for Neo4j connector");
+            System.err.println(ex.getMessage());
+        }
 
         return 23;
     }
