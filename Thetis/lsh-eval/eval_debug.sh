3ce1bcd217a0292fe29ffac1416556b70ae7857a
Matteo @ Nova2 <matteo@cs.aau.dk>
2022-11-01 10:42:46 +0000

    Evaluate column aggregation for LSH of types

--- Thetis/lsh-eval/eval_debug.sh
+++ Thetis/lsh-eval/eval_debug.sh
@@ -64,31 +64,54 @@ do
         mkdir -p ${OUT_K}
 
         java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
+            -q ${QUERIES_DIR} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_TYPES --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
+    done
+done
+
+for I in "/src/lsh-eval/aggregation/"* ; \
+do
+    SPLIT=(${I//_/ })
+    VECTORS=${SPLIT[-3]}
+    BAND_SIZE=${SPLIT[-1]}
+    OUT=/src/lsh-eval/results/agregation/vectors_${VECTORS}/bandsize_${BAND_SIZE}
+    mkdir -p ${OUT}
+
+    echo "PROJECTION VECTORS (COLUMN AGGREGATION): "${VECTORS}
+    echo
+
+    for TOP_K in {10,100} ; \
+    do
+        OUT_K=${OUT}/${TOP_K}
+        mkdir -p ${OUT_K}
+
+        java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
             -q ${QUERIES_DIR} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_EMBEDDINGS --singleColumnPerQueryEntity --usePretrainedEmbeddings --useMaxSimilarityPerColumn --embeddingSimilarityFunction norm_cos
     done
 done
 
-#echo "BASELINE"
-#OUT=${OUTPUT_DIR}baseline_jaccard/vectors_32
-#mkdir -p ${OUT}
+OUTPUT_DIR='/src/lsh-eval/results/baseline/'
 
-#for TOP_K in {10,100} ; \
-#do
-#    OUT_K=${OUT}/${TOP_K}
-#    mkdir -p ${OUT_K}
+echo "BASELINE"
+OUT=${OUTPUT_DIR}baseline_jaccard/vectors_32
+mkdir -p ${OUT}
 
-#    java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_32_bandsize_8 \
-#        -q ${QUERIES_DIR} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
-#done
+for TOP_K in {10,100} ; \
+do
+    OUT_K=${OUT}/${TOP_K}
+    mkdir -p ${OUT_K}
 
-#OUT=${OUTPUT_DIR}baseline_cosine/vectors_32
-#mkdir -p ${OUT}
+    java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_32_bandsize_8 \
+        -q ${QUERIES_DIR} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
+done
 
-#for TOP_K in {10,100} ; \
-#do
-#    OUT_K=${OUT}/${TOP_K}
-#    mkdir -p ${OUT_K}
+OUT=${OUTPUT_DIR}baseline_cosine/vectors_32
+mkdir -p ${OUT}
 
-#    java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_32_bandsize_8 \
-#        -q ${QUERIES_DIR} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --usePretrainedEmbeddings --useMaxSimilarityPerColumn --embeddingSimilarityFunction norm_cos
-#done
+for TOP_K in {10,100} ; \
+do
+    OUT_K=${OUT}/${TOP_K}
+    mkdir -p ${OUT_K}
+
+    java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_32_bandsize_8 \
+        -q ${QUERIES_DIR} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --usePretrainedEmbeddings --useMaxSimilarityPerColumn --embeddingSimilarityFunction norm_cos
+done
