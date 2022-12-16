83e9e8cdf8d9d2c34186fcc7fcfad40f393660cb
JM14OK <jm14ok@cs.aau.dk@a2048-gc1-02.srv.aau.dk>
2022-12-16 11:26:25 +0100

    Renaming

--- Thetis/lsh-eval/eval.sh
+++ Thetis/lsh-eval/eval.sh
@@ -1,121 +1,142 @@
 #!/bin/bash
 
-INDEX_DIR="/data/cikm/indexes/"
+INDEX_DIR="/src/lsh-eval/indexes/"
 TABLES="/data/cikm/SemanticTableSearchDataset/table_corpus/corpus/"
-OUTPUT_DIR="/src/lsh-eval/results_full/"
-QUERIES_DIR="/data/cikm/SemanticTableSearchDataset/queries/"
-
-# Run for each bucket configuration using LSH of entity types
-#echo "Evalution of LSH pre-filtering using entity types..."
-#echo
+OUTPUT_DIR='/src/lsh-eval/results/vote_1/'
+QUERIES_DIR="/src/lsh-eval/queries/"
+TOP_K=100
 
 for I in ${INDEX_DIR}* ; \
 do
     SPLIT=(${I//_/ })
     VECTORS=${SPLIT[-3]}
-    OUT=${OUTPUT_DIR}types/vectors_${VECTORS}
+    BAND_SIZE=${SPLIT[-1]}
+    OUT=${OUTPUT_DIR}types/vectors_${VECTORS}/bandsize_${BAND_SIZE}
     mkdir -p ${OUT}
 
     echo "PERMUTATION VECTORS: "${VECTORS}
     echo
 
-    for QUERY_DIR in ${QUERIES_DIR}*_tuples_per_query ; \
+    for TUPLES in {1,2,5} ; \
     do
-        SPLIT1=(${QUERY_DIR//"/"/ })
-        SPLIT2=(${SPLIT1[-1]//_/ })
-        TUPLES=${SPLIT2[0]}
-        OUT_TUPLES=${OUT}/${TUPLES}_tuple_queries
-        mkdir -p ${OUT_TUPLES}
+        OUT_K=${OUT}/${TOP_K}/${TUPLES}-tuple/
+        mkdir -p ${OUT_K}
 
-        for TOP_K in {10,100} ; \
-        do
-            OUT_K=${OUT_TUPLES}/${TOP_K}
-            mkdir -p ${OUT_K}
-
-            java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
-                -q ${QUERY_DIR} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_TYPES --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
-        done
+        QUERIES=${QUERIES_DIR}${TUPLES}-tuple/
+        java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
+            -q ${QUERIES} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_TYPES --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
     done
 done
-
-# Run for each bucket configuration using LSH of entity embeddings
-echo "Evalution of LSH pre-filtering using entity embeddings..."
-echo
 
 for I in ${INDEX_DIR}* ; \
 do
     SPLIT=(${I//_/ })
     VECTORS=${SPLIT[-3]}
-    OUT=${OUTPUT_DIR}embeddings/vectors_${VECTORS}
+    BAND_SIZE=${SPLIT[-1]}
+    OUT=${OUTPUT_DIR}embeddings/vectors_${VECTORS}/bandsize_${BAND_SIZE}
     mkdir -p ${OUT}
 
     echo "PROJECTION VECTORS: "${VECTORS}
     echo
 
-    for QUERY_DIR in ${QUERIES_DIR}*_tuples_per_query ; \
+    for TUPLES in {1,2,5} ; \
     do
-        SPLIT1=(${QUERY_DIR//"/"/ })
-        SPLIT2=(${SPLIT1[-1]//_/ })
-        TUPLES=${SPLIT2[0]}
-        OUT_TUPLES=${OUT}/${TUPLES}_tuple_queries
-        mkdir -p ${OUT_TUPLES}
-
-        for TOP_K in {10,100} ; \
-        do
-            OUT_K=${OUT_TUPLES}/${TOP_K}
-            mkdir -p ${OUT_K}
-
-            java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
-                -q ${QUERY_DIR} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_EMBEDDINGS --singleColumnPerQueryEntity --useMaxSimilarityPerColumn --usePretrainedEmbeddings --embeddingSimilarityFunction norm_cos
-        done
-    done
-done
-
-# Run baseline: Without using pre-filtering
-echo "Evalution of baseline (Jaccard) without using LSH pre-filtering..."
-echo
-
-OUT=${OUTPUT_DIR}baseline_jaccard/vectors_32
-mkdir -p ${OUT_BUCKETS}
-
-for QUERY_DIR in ${QUERIES_DIR}*_tuples_per_query ; \
-do
-    SPLIT1=(${QUERY_DIR//"/"/ })
-    SPLIT2=(${SPLIT1[-1]//_/ })
-    TUPLES=${SPLIT2[0]}
-    OUT_TUPLES=${OUT_BUCKETS}/${TUPLES}_tuple_queries
-    mkdir -p ${OUT_TUPLES}
-
-    for TOP_K in {10,100} ; \
-    do
-        OUT_K=${OUT_TUPLES}/${TOP_K}
+        OUT_K=${OUT}/${TOP_K}/${TUPLES}-tuple/
         mkdir -p ${OUT_K}
 
-        java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_32_bandsize_4 \
-            -q ${QUERY_DIR} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
+        QUERIES=${QUERIES_DIR}${TUPLES}-tuple/
+        java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
+            -q ${QUERIES} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_EMBEDDINGS --singleColumnPerQueryEntity --usePretrainedEmbeddings --useMaxSimilarityPerColumn --embeddingSimilarityFunction norm_cos
     done
 done
 
-echo "Evalution of baseline (Cosine) without using LSH pre-filtering..."
-echo
-
-OUT=${OUTPUT_DIR}baseline_cosine/vectors_32
-mkdir -p ${OUT_BUCKETS}
-
-for QUERY_DIR in ${QUERIES_DIR}*_tuples_per_query ; \
+for I in "/src/lsh-eval/indexes_aggregation/"* ; \
 do
-    SPLIT1=(${QUERY_DIR//"/"/ })
-    SPLIT2=(${SPLIT1[-1]//_/ })
-    TUPLES=${SPLIT2[0]}
-    OUT_TUPLES=${OUT_BUCKETS}/${TUPLES}_tuple_queries
-    mkdir -p ${OUT_TUPLES}
+    SPLIT=(${I//_/ })
+    VECTORS=${SPLIT[-3]}
+    BAND_SIZE=${SPLIT[-1]}
+    OUT=${OUTPUT_DIR}aggregation/types/vectors_${VECTORS}/bandsize_${BAND_SIZE}
+    mkdir -p ${OUT}
 
-    for TOP_K in {10,100} ; \
+    echo "PROJECTION VECTORS (COLUMN AGGREGATION): "${VECTORS}
+    echo
+
+    for TUPLES in {1,2,5} ; \
     do
-        OUT_K=${OUT_TUPLES}/${TOP_K}
+        OUT_K=${OUT}/${TOP_K}/${TUPLES}-tuple/
         mkdir -p ${OUT_K}
 
-        java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_32_bandsize_4 \
-            -q ${QUERY_DIR} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --usePretrainedEmbeddings --useMaxSimilarityPerColumn --embeddingSimilarityFunction norm_cos
+        QUERIES=${QUERIES_DIR}${TUPLES}-tuple/
+        java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
+            -q ${QUERIES} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_TYPES --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
     done
 done
+
+for I in "/src/lsh-eval/indexes_aggregation/"* ; \
+do
+    SPLIT=(${I//_/ })
+    VECTORS=${SPLIT[-3]}
+    BAND_SIZE=${SPLIT[-1]}
+    OUT=${OUTPUT_DIR}aggregation/embeddings/vectors_${VECTORS}/bandsize_${BAND_SIZE}
+    mkdir -p ${OUT}
+
+    echo "PROJECTION VECTORS (COLUMN AGGREGATION): "${VECTORS}
+    echo
+
+    for TUPLES in {1,2,5} ; \
+    do
+        OUT_K=${OUT}/${TOP_K}/${TUPLES}-tuple/
+        mkdir -p ${OUT_K}
+
+        QUERIES=${QUERIES_DIR}${TUPLES}-tuple/
+        java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${I} \
+            -q ${QUERIES} -td ${TABLES} -od ${OUT_K} -t 4 -pf LSH_EMBEDDINGS --singleColumnPerQueryEntity --usePretrainedEmbeddings --useMaxSimilarityPerColumn --embeddingSimilarityFunction norm_cos
+    done
+done
+
+OUTPUT_DIR='/src/lsh-eval/results/baseline/'
+
+echo "BASELINE - PURE BRUTE FORCE"
+OUT=${OUTPUT_DIR}baseline_jaccard
+mkdir -p ${OUT}
+
+for TUPLES in {1,2,5} ; \
+do
+    OUT_K=${OUT}/${TOP_K}/${TUPLES}-tuple/
+    mkdir -p ${OUT_K}
+
+    QUERIES=${QUERIES_DIR}${TUPLES}-tuple/
+    java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_30_bandsize_10 \
+        -q ${QUERIES} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --adjustedJaccardSimilarity --useMaxSimilarityPerColumn
+done
+
+OUT=${OUTPUT_DIR}baseline_cosine
+mkdir -p ${OUT}
+
+for TUPLES in {1,2,5} ; \
+do
+    OUT_K=${OUT}/${TOP_K}/${TUPLES}-tuple/
+    mkdir -p ${OUT_K}
+
+    QUERIES=${QUERIES_DIR}${TUPLES}-tuple/
+    java -Xmx55g -jar target/Thetis.0.1.jar search --search-mode analogous -topK ${TOP_K} -i ${INDEX_DIR}vectors_30_bandsize_10 \
+        -q ${QUERIES} -td ${TABLES} -od ${OUT_K} -t 4 --singleColumnPerQueryEntity --usePretrainedEmbeddings --useMaxSimilarityPerColumn --embeddingSimilarityFunction norm_cos
+done
+
+#echo "BASELINE - BM25"
+#OUT=${OUTPUT_DIR}baseline_bm25
+#INDEX_NAME='bm25'
+#mkdir -p ${OUT}
+
+#for TOP_K in {10,100} ; \
+#do
+#    for TUPLES in {1,2,5} ; \
+#    do
+#        OUT_K=${OUT}/${TOP_K}/${TUPLES}-tuple/
+#        mkdir -p ${OUT_K}
+
+#        QUERIES=${QUERIES_DIR}bm25_${TUPLES}-tuple/queries.txt
+#        python3 bm25/pool_ranker.py --output_dir ${OUT_K} \
+#            --input_dir ${QUERIES} --index_name ${INDEX_NAME} --topn ${TOP_K}
+#    done
+#done
