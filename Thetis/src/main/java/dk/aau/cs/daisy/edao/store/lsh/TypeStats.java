82303bb3748dc9cd0a916207e3798b608908f1f4
MrPekar98 <martin.pekar@outlook.dk>
2023-01-03 16:19:30 +0100

    Compute type frequency in tables

--- Thetis/src/main/java/dk/aau/cs/daisy/edao/store/lsh/TypeStats.java
+++ Thetis/src/main/java/dk/aau/cs/daisy/edao/store/lsh/TypeStats.java
@@ -1,10 +1,13 @@
 package dk.aau.cs.daisy.edao.store.lsh;
 
+import dk.aau.cs.daisy.edao.store.EntityLinking;
 import dk.aau.cs.daisy.edao.store.EntityTable;
 import dk.aau.cs.daisy.edao.structures.Id;
 import dk.aau.cs.daisy.edao.structures.graph.Type;
+import dk.aau.cs.daisy.edao.structures.table.Table;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public final class TypeStats
 {
@@ -15,6 +18,11 @@ public final class TypeStats
         this.entityTable = entityTable;
     }
 
+    /**
+     * The types included in the percentile of type frequency among entities.
+     * @param percentile Between 0 and 1.0. E.g., 90th percentile is 0.9.
+     * @return The most frequent types for entities within a given percentile.
+     */
     public Set<String> popularByPercentile(double percentile)
     {
         if (percentile < 0 || percentile > 1)
@@ -62,5 +70,93 @@ public final class TypeStats
         }
 
         return count;
+    }
+
+    /**
+     * Most popular types according to type percentages in tables.
+     * @param percentage Determines the percentage within which the type must be to be included.
+     * @param tables Corpus of tables to compute percentages from.
+     * @param linker To perform lookup from entity string to its numeric ID
+     * @return Set of types included in the top-K most frequent types found in tables, where K is a given percentage.
+     */
+    public Set<String> popularByTable(double percentage, Set<Table<String>> tables, EntityLinking linker)
+    {
+        Set<String> types = countOccurrences().keySet();
+        Map<String, Integer> typeCountInTables = new HashMap<>();
+
+        for (String type : types)
+        {
+            for (Table<String> table : tables)
+            {
+                if (hasType(table, type, linker))
+                {
+                    typeCountInTables.put(type,
+                            typeCountInTables.containsKey(type) ? typeCountInTables.get(type) + 1 : 1);
+                }
+            }
+        }
+
+        Map<String, Double> typePercentages = percentages(typeCountInTables);
+        List<Map.Entry<String, Double>> sorted = new ArrayList<>(typePercentages.entrySet());
+        sorted.sort(Comparator.comparingDouble(Map.Entry::getValue));
+
+        List<String> sortedTypes = sorted.stream()
+                .filter(pair -> pair.getValue() >= percentage)
+                .map(Map.Entry::getKey)
+                .toList();
+        return new HashSet<>(sortedTypes);
+    }
+
+    private boolean hasType(Table<String> table, String type, EntityLinking linker)
+    {
+        int rows = table.rowCount();
+
+        for (int row = 0; row < rows; row++)
+        {
+            int columns = table.getRow(row).size();
+
+            for (int column = 0; column < columns; column++)
+            {
+                String entity = linker.mapTo(table.getRow(row).get(column));
+
+                if (entity == null)
+                {
+                    continue;
+                }
+
+                Id id = linker.kgUriLookup(entity);
+
+                if (id != null)
+                {
+                    Set<String> entityTypes = this.entityTable.find(id)
+                            .getTypes()
+                            .stream()
+                            .map(Type::getType)
+                            .collect(Collectors.toSet());
+
+
+                    if (entityTypes.contains(type))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static <E> Map<E, Double> percentages(Map<E, Integer> map)
+    {
+        int count = map.size();
+        Map<E, Double> percentages = new HashMap<>(map.size());
+
+        for (Map.Entry<E, Integer> entry : map.entrySet())
+        {
+            double fraction = (double) entry.getValue() / count;
+            percentages.put(entry.getKey(), fraction);
+        }
+
+        return percentages;
     }
 }
--- Thetis/src/test/java/dk/aau/cs/daisy/edao/store/lsh/TypeStatsTest.java
+++ Thetis/src/test/java/dk/aau/cs/daisy/edao/store/lsh/TypeStatsTest.java
@@ -1,12 +1,17 @@
 package dk.aau.cs.daisy.edao.store.lsh;
 
+import dk.aau.cs.daisy.edao.store.EntityLinking;
 import dk.aau.cs.daisy.edao.store.EntityTable;
 import dk.aau.cs.daisy.edao.structures.Id;
 import dk.aau.cs.daisy.edao.structures.graph.Entity;
 import dk.aau.cs.daisy.edao.structures.graph.Type;
+import dk.aau.cs.daisy.edao.structures.table.DynamicTable;
+import dk.aau.cs.daisy.edao.structures.table.Table;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertFalse;
@@ -15,17 +20,31 @@ import static org.junit.Assert.assertTrue;
 public class TypeStatsTest
 {
     private final EntityTable entTable = new EntityTable();
+    private final EntityLinking linker = new EntityLinking("", "");
     private final Id id1 = Id.alloc(), id2 = Id.alloc(), id3 = Id.alloc();
-    Entity ent1 = new Entity("uri1", new Type("type1"), new Type("type2"), new Type("type3")),
+    private final String tableEnt1 = "ent1", tableEnt2 = "ent2", tableEnt3 = "ent3";
+    private final Entity ent1 = new Entity("uri1", new Type("type1"), new Type("type2"), new Type("type3")),
             ent2 = new Entity("uri2", new Type("type2")),
             ent3 = new Entity("uri3", new Type("type1"), new Type("type2"));
+
+    private final Set<Table<String>> corpus = new HashSet<>();
 
     @Before
     public void init()
     {
-        this.entTable.insert(this.id1, this.ent1);
-        this.entTable.insert(this.id2, this.ent2);
-        this.entTable.insert(this.id3, this.ent3);
+        this.linker.addMapping(this.tableEnt1, this.ent1.getUri());
+        this.linker.addMapping(this.tableEnt2, this.ent2.getUri());
+        this.linker.addMapping(this.tableEnt3, this.ent3.getUri());
+
+        this.entTable.insert(this.linker.kgUriLookup(this.ent1.getUri()), this.ent1);
+        this.entTable.insert(this.linker.kgUriLookup(this.ent2.getUri()), this.ent2);
+        this.entTable.insert(this.linker.kgUriLookup(this.ent3.getUri()), this.ent3);
+
+        Table<String> t1 = new DynamicTable<>(List.of(List.of(this.tableEnt1, this.tableEnt2),
+                List.of(this.tableEnt2, this.tableEnt3))),
+                t2 = new DynamicTable<>(List.of(List.of(this.tableEnt1))),
+                t3 = new DynamicTable<>(List.of(List.of(this.tableEnt2, this.tableEnt3)));
+        this.corpus.addAll(Set.of(t1, t2, t3));
     }
 
     @Test
@@ -54,5 +73,35 @@ public class TypeStatsTest
         assertTrue(some.contains("type1"));
         assertTrue(some.contains("type2"));
         assertFalse(some.contains("type3"));
+    }
+
+    @Test
+    public void testTablePercentageAll()
+    {
+        TypeStats stats = new TypeStats(this.entTable);
+        Set<String> types = stats.popularByTable(0.0, this.corpus, this.linker);
+        assertTrue(types.contains("type1"));
+        assertTrue(types.contains("type2"));
+        assertTrue(types.contains("type3"));
+    }
+
+    @Test
+    public void testTablePercentageSome()
+    {
+        TypeStats stats = new TypeStats(this.entTable);
+        Set<String> types = stats.popularByTable(0.8, this.corpus, this.linker);
+        assertTrue(types.contains("type1"));
+        assertTrue(types.contains("type2"));
+        assertFalse(types.contains("type3"));
+    }
+
+    @Test
+    public void testTablePercentageNone()
+    {
+        TypeStats stats = new TypeStats(this.entTable);
+        Set<String> types = stats.popularByTable(1.0, this.corpus, this.linker);
+        assertTrue(types.contains("type1"));
+        assertTrue(types.contains("type2"));
+        assertFalse(types.contains("type3"));
     }
 }
