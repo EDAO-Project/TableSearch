0dacc86660994de0bb0c0701cb0fad9f530a80b2
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2023-01-26 10:27:19 +0100

    Plotting of BM25 as pre-filter

--- Thetis/lsh-eval/plot.py
+++ Thetis/lsh-eval/plot.py
@@ -57,7 +57,7 @@ def ground_truth(query_filename, ground_truth_folder, table_corpus_folder, pickl
     return query, relevances
 
 # Returns None if results for given query ID do not exist
-def predicted_scores(query_id, votes, mode, vectors, band_size, tuples, k, gt_tables, is_baseline = False, is_column_aggregation = False, is_bm25 = False):
+def predicted_scores(query_id, votes, mode, vectors, band_size, tuples, k, gt_tables, is_baseline = False, is_column_aggregation = False, is_bm25 = False, is_bm25_prefilter = False):
     path = 'results/vote_' + str(votes) + '/' + mode + '/vectors_' + str(vectors) + '/bandsize_' + str(band_size) + '/' + str(k) + '/' + str(tuples) + '-tuple/search_output/' + query_id + '/filenameToScore.json'
 
     if (is_baseline):
@@ -68,6 +68,9 @@ def predicted_scores(query_id, votes, mode, vectors, band_size, tuples, k, gt_ta
 
     if (is_bm25):
         path = 'results/baseline/bm25/' + str(k) + '/' + str(tuples) + '-tuple/' + mode + '/content.txt'
+
+    elif (is_bm25_prefilter):
+        path = 'results/baseline/baseline_bm25_prefiltering/' + mode + '/' str(k) + '/' str(tuples) + '-tuple/search_output/' + query_id + '/filenameToScore.json'
 
     if not os.path.exists(path):
         return None
@@ -108,6 +111,8 @@ def full_corpus(base_dir):
     return tables
 
 def gen_boxplots(ndcg_dict, votes, tuples):
+    # First the plots of all configurations
+
     plt.rc('xtick', labelsize = 25)
     plt.rc('ytick', labelsize = 35)
     plt.rc('axes', labelsize = 27)
@@ -160,6 +165,52 @@ def gen_boxplots(ndcg_dict, votes, tuples):
     plt.savefig(str(tuples) + '-tuple_plot_' + str(votes) + '_votes.pdf', format = 'pdf')
     plt.clf()
 
+    # Plotting BM25 as a pre-filtering technique and comparing it to all configurations without aggregation
+
+    plt.rc('xtick', labelsize = 25)
+    plt.rc('ytick', labelsize = 35)
+    plt.rc('axes', labelsize = 27)
+    plt.rc('axes', titlesize = 30)
+    plt.rc('legend', fontsize = 25)
+
+    colors = ['lightblue', 'blue', 'lightgreen', 'brown']
+    fig, (ax1, ax2) = plt.subplots(nrows = 1, ncols = 2, figsize = (60, 10))
+    median_color = dict(color = 'white')
+    data_types = list()
+    data_embeddings = list()
+
+    data_types.append(ndcg_dict[str(votes)]['types']['32']['8'])
+    data_types.append(ndcg_dict[str(votes)]['types']['128']['8'])
+    data_types.append(ndcg_dict[str(votes)]['types']['30']['10'])
+    data_types.append(ndcg_dict['baseline']['bm25_prefilter']['types'])
+    data_types.append(ndcg_dict['baseline']['jaccard'])
+
+    data_embeddings.append(ndcg_dict[str(votes)]['types']['32']['8'])
+    data_embeddings.append(ndcg_dict[str(votes)]['types']['128']['8'])
+    data_embeddings.append(ndcg_dict[str(votes)]['types']['30']['10'])
+    data_embeddings.append(ndcg_dict['baseline']['bm25_prefilter']['embeddings'])
+    data_embeddings.append(ndcg_dict['baseline']['jaccard'])
+
+    plot_types = ax1.boxplot(data_types, vert = True, patch_artist = True, medianprops = median_color, labels = ['T(V=32, BS=8)', 'T(V=128, BS=8)', 'T(V=30, BS=10)', 'BM25 pre-filtering - Jaccard', 'B - Jaccard'])
+    ax1.set_title('LSH Using Types')
+
+    plot_types = ax1.boxplot(data_types, vert = True, patch_artist = True, medianprops = median_color, labels = ['E(V=32, BS=8)', 'E(V=128, BS=8)', 'E(V=30, BS=10)', 'BM25 pre-filtering - cosine', 'B - cosine'])
+    ax2.set_title('LSH Using Embeddings')
+
+    for plot in (plot_types, plot_embeddings):
+        for patch, color in zip(plot['boxes'], colors):
+            patch.set_facecolor(color)
+
+    ax1.set_xlabel('T = LSH of types, E = LSH of embeddings, B = Brute-Force V = # permutation/projection vectors, BS = band size')
+
+    for ax in [ax1, ax2]:
+        ax.yaxis.grid(True)
+        ax.set_ylabel('NDCG')
+        ax.vlines(3.5, 0, 1.0)
+
+    plt.savefig(str(tuples) + '-tuple_plot_' + str(votes) + '_votes_bm25_prefilter.pdf', format = 'pdf')
+    plt.clf()
+
 # Returns map: ['types'|'embeddings'|'baseline']->[<# BUCKETS: [150|300]>]->[<TOP-K: [10|100]>]->[NDCG SCORES]
 def plot_ndcg(tuples):
     query_dir = 'queries/' + str(tuples) + '-tuple/'
@@ -208,6 +259,9 @@ def plot_ndcg(tuples):
         ndcg['baseline']['cosine'] = list()
         ndcg['baseline']['bm25_entities'] = list()
         ndcg['baseline']['bm25_text'] = list()
+        ndcg['baseline']['bm25_prefilter'] = dict()
+        ndcg['baseline']['bm25_prefilter']['types'] = list()
+        ndcg['baseline']['bm25_prefilter']['embeddings'] = list()
 
         count = 0
         print('Vote = ' + str(vote))
@@ -318,17 +372,23 @@ def plot_ndcg(tuples):
                 ndcg_baseline = ndcg_score(np.array([list(gt_rels.values())]), np.array([predicted_relevance]), k = k)
                 ndcg['baseline']['bm25_entities'].append(ndcg_baseline)
 
-            else:
-                print('Failed entities')
-
             predicted_relevance = predicted_scores(query_id, vote, 'text', 32, 8, tuples, k, gt_rels, True, False, True)
 
             if not predicted_relevance is None:
                 ndcg_baseline = ndcg_score(np.array([list(gt_rels.values())]), np.array([predicted_relevance]), k = k)
                 ndcg['baseline']['bm25_text'].append(ndcg_baseline)
 
-            else:
-                print('Failed text')
+            predicted_relevance = predicted_scores(query_id, vote, 'types', 32, 8, tuples, k, gt_rels, False, False, False, True)
+
+            if not predicted_relevance is None:
+                ndcg_baseline = ndcg_score(np.array([list(gt_rels.values())]), np.array([predicted_relevance]), k = k)
+                ndcg['baseline']['bm25_prefilter']['types'].append(ndcg_baseline)
+
+            predicted_relevance = predicted_scores(query_id, vote, 'embeddings', 32, 8, tuples, k, gt_rels, False, False, False, True)
+
+            if not predicted_relevance is None:
+                ndcg_baseline = ndcg_score(np.array([list(gt_rels.values())]), np.array([predicted_relevance]), k = k)
+                ndcg['baseline']['bm25_prefilter']['embeddings'].append(ndcg_baseline)
 
         gen_boxplots(ndcg, vote, tuples)
 
