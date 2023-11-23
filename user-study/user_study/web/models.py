03a6c58d4280135edce65996305fc9746d5b977c
MrPekar98 <martin.pekar@outlook.dk>
2023-11-23 14:38:38 +0100

    Update

--- user-study/user_study/web/models.py
+++ user-study/user_study/web/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Represents a user
 class User(models.Model):
+    id = models.BigAutoField(primary_key = True)
     username = models.CharField(max_length = 255, default = '')       # Username
 
 # Represents a table for annotation
 class Table(models.Model):
+    id = models.BigAutoField(primary_key = True)
     name = models.CharField(max_length = 255, default = '')    # Name of query
 
 # Represents an input query
 class Query(models.Model):
+    id = models.BigAutoField(primary_key = True)
     name = models.CharField(max_length = 255, default = '')            # Name of query
 
 # Represents an annotation for a query and table
@@ -21,11 +24,13 @@ class Annotation(models.Model):
 
 # Represents a table annotation task
 class Task(models.Model):
+    id = models.BigAutoField(primary_key = True)
     query = models.ForeignKey(Query, to_field = 'id', on_delete = models.CASCADE, default = -1) # Reference to Query
     tables = models.CharField(max_length = 1024, default = '')                                # Array of table names for this task
 
 # Represents some work, where a user has been assigned a query and a table to annotate
 class Work(models.Model):
+    id = models.BigAutoField(primary_key = True)
     user = models.ForeignKey(User, to_field = 'id', on_delete = models.CASCADE, default = -1)   # Reference to user
     task = models.ForeignKey(Task, to_field = 'id', on_delete = models.CASCADE, default = -1)   # Reference to Task
 
